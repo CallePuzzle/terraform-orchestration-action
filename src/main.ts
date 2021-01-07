53697ef870191cfce4e1a4e0e7819841f1268fb7
César M. Cristóbal <cesar@callepuzzle.com>
2021-01-07 20:38:12 +0100

    Added runTerraform.test

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ interface Input {
     workingDirectory: string
     baseRef: string
     headRef: string
+    workspace: string | undefined
+    apply: boolean
 }
 
 export const main = (input: Input): void => {
@@ -16,7 +18,7 @@ export const main = (input: Input): void => {
         getGitModifiedDirectories(input.workingDirectory, input.baseRef, input.headRef)
             .then(r => {
                 r.map(componentPath => {
-                    runTerraform(processCwd, componentPath)
+                    runTerraform(processCwd, componentPath, input.workspace, input.apply)
                 })
             })
     }).catch(e => {
@@ -30,6 +32,8 @@ try {
         workingDirectory: path.join('__tests__', 'tf_project', '/'), //core.getInput('workingDirectory'),
         baseRef: '86e35d7e707289588dcf6f825be3e7c3e854cf58', //core.getInput('baseRef'),
         headRef: '009ec5a0e9196b668d926916eaeb6f778694074f', //core.getInput('headRef')
+        workspace: core.getInput('workspace'),
+        apply: core.getInput('apply') === "true"
     })
 } catch (error) {
     core.setFailed(error)
--- src/runTerraform.ts
+++ src/runTerraform.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import { spawnSync } from 'child_process'
 import path from 'path'
 
-export const runTerraform = (processCwd: string, componentPath: string, workspace: string | undefined): boolean => {
+export const runTerraform = (processCwd: string, componentPath: string, workspace: string | undefined, apply: boolean): boolean => {
     process.chdir(path.join(processCwd, componentPath));
     if (!setWorkspace(workspace))
         return false
@@ -10,25 +10,27 @@ export const runTerraform = (processCwd: string, componentPath: string, workspac
     const options = [
         ['validate'],
         ['plan', '-out=plan'],
-        ['apply', 'plan']
     ]
+    if (apply)
+        options.push(['apply', 'plan'])
+
     for (const option of options) {
         const run = spawnSync('terraform', option)
         core.info(run.output[1])
         core.info(run.output[2])
         if (run.status !== 0) {
-            break
+            return false
         }
     }
     return true
 }
 
 export const setWorkspace = (workspace: string | undefined): boolean => {
-    if (workspace) {
-        const run = spawnSync('terraform', ['workspace', 'select', workspace])
-        core.info(run.output[1])
-        core.info(run.output[2])
-        return run.status === 0
-    } else
-        return false
+    if (workspace === undefined)
+        return true
+
+    const run = spawnSync('terraform', ['workspace', 'select', workspace])
+    core.info(run.output[1])
+    core.info(run.output[2])
+    return run.status === 0
 }
