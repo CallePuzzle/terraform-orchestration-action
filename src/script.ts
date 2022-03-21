6f6ce7632d6137f76f100bccf76ec6b9276cca9b
César M. Cristóbal <cesar@callepuzzle.com>
2022-03-21 20:55:47 +0100

    Fix excludeDirectories in action test

--- src/script.ts
+++ src/script.ts
@@ -9,10 +9,10 @@ class Log implements LogInterface {
 
 try {
     main({
-        workingDirectory: '.',//'__tests__/tf_project/', //core.getInput('workingDirectory'),
-        baseRef: '2d601a8c38ef73231a2fba9dca2b5c26d304d9a3', //core.getInput('baseRef'),
-        headRef: 'fa666c95a0a4598b71036de75163fc68bfa4e2c4', //core.getInput('headRef')
-        excludeDirectories: [],
+        workingDirectory: '__tests__/tf_project/', //core.getInput('workingDirectory'),
+        baseRef: '86e35d7e707289588dcf6f825be3e7c3e854cf58', //core.getInput('baseRef'),
+        headRef: '009ec5a0e9196b668d926916eaeb6f778694074f', //core.getInput('headRef')
+        excludeDirectories: ['__tests__/tf_project/010-second'],
         workspace: undefined,
         apply: core.getInput('apply') === "true"
     }, new Log())
