47b70a8417292d7794fb25bfa4704b152b89c82a
César M. Cristóbal <cesar@callepuzzle.com>
2022-03-21 20:45:13 +0100

    fix excludeDirectories type

--- src/script.ts
+++ src/script.ts
@@ -12,7 +12,7 @@ try {
         workingDirectory: '.',//'__tests__/tf_project/', //core.getInput('workingDirectory'),
         baseRef: '2d601a8c38ef73231a2fba9dca2b5c26d304d9a3', //core.getInput('baseRef'),
         headRef: 'fa666c95a0a4598b71036de75163fc68bfa4e2c4', //core.getInput('headRef')
-        excludeDirectories: undefined,
+        excludeDirectories: [],
         workspace: undefined,
         apply: core.getInput('apply') === "true"
     }, new Log())
